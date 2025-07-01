214e42358e77419d99a88ae2ede0ab9fddcdd8b2
email21 <steady.challenger@gmail.com>
2025-07-01 19:12:14 +0900

    add

--- organize_log_files.py
+++ organize_log_files.py
@@ -16,7 +16,7 @@ error_lines = [line for line in lines if line[0:7] == "[ERROR]" ]
 
 # 추가 - info, warning 에 대해서도 따로 저장하기
 info_lines = [line for line in lines if line.startswith("[INFO]")]
-warning_lines = [line for line in lines if line.startswith("[warning]")]
+warning_lines = [line for line in lines if line.startswith("[WARNING]")] # 대소문자 구분함
 
 # 3. 추출한 내용 파일에 쓰기
 with open("error_log.txt","w") as f: # w : 쓰기 모드 (파일이 없으면 생성, 기존 파일이 있으면 덮어씀)
