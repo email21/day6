e95b4c7a84d1c8cc2335a24945716fe237d905e5
email21 <steady.challenger@gmail.com>
2025-07-01 19:11:06 +0900

    add info, warning

--- organize_log_files.py
+++ organize_log_files.py
@@ -14,6 +14,16 @@ except FileNotFoundError:
 # 2. 에러 추출하기
 error_lines = [line for line in lines if line[0:7] == "[ERROR]" ]
 
+# 추가 - info, warning 에 대해서도 따로 저장하기
+info_lines = [line for line in lines if line.startswith("[INFO]")]
+warning_lines = [line for line in lines if line.startswith("[warning]")]
+
 # 3. 추출한 내용 파일에 쓰기
 with open("error_log.txt","w") as f: # w : 쓰기 모드 (파일이 없으면 생성, 기존 파일이 있으면 덮어씀)
     f.writelines(error_lines)
+    
+with open("info_log.txt","w") as f:
+    f.writelines(info_lines)
+    
+with open("warning_log.txt", "w") as f:
+    f.writelines(warning_lines)
